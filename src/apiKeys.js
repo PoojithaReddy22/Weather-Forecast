da7d1786b069bf022be71e7d47b93c15bf57304f
PoojithaReddy22 <100334402+PoojithaReddy22@users.noreply.github.com>
2023-08-11 10:02:18 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ce911c46b1befdbeb732fbd613b345d7",
   base: "https://api.openweathermap.org/data/2.5/",
 };
